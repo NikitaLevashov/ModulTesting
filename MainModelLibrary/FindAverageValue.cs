3e9c2ede722a9cc10d033e4e748f3e29eeea579e
levashov_92 <levashov_92@outlook.com>
2020-11-28 00:30:50 +0300

    Create class FindAverageValue

---
+++ MainModelLibrary/FindAverageValue.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MainModelLibrary
+{
+    public static class FindAverageValue
+    {
+        public static double GetAverage(int[] array)
+        {
+            int sum = 0;
+
+            for(int i=0;i<array.Length;i++)
+            {
+                sum += array[i];
+            }
+
+            return (double)sum / array.Length;
+        }
+
+
+    }
+}
