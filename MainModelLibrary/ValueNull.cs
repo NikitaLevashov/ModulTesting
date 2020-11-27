8ffa57813549d5660c60fc3f22ffe471d14810e1
levashov_92 <levashov_92@outlook.com>
2020-11-28 00:39:47 +0300

    Create class ValueNull

---
+++ MainModelLibrary/ValueNull.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MainModelLibrary
+{
+    public static class ValueNull
+    {
+        public static string MethodFindNull(string value)
+        {
+            if(value == null)
+            {
+                throw new NullReferenceException("Value null");
+            }
+
+            return value;
+
+        }
+
+    }
+}
