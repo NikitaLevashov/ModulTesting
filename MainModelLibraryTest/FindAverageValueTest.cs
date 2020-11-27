d610104c62f0d506d71635e017a7f4aa0700fa8b
levashov_92 <levashov_92@outlook.com>
2020-11-28 01:05:04 +0300

    Create FindAverageValueTest

---
+++ MainModelLibraryTest/FindAverageValueTest.cs
@@ -0,0 +1,26 @@
+﻿using MainModelLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MainModelLibraryTest
+{
+    [TestClass]
+    public static class FindAverageValueTest
+    {
+        [TestMethod]
+        public static void GetAverageElements_Test()
+        {
+            //Arrange
+            int[] array = { 2, 5, 3 };
+
+            //Act
+            double result = FindAverageValue.GetAverage(array);
+
+            //Assert
+            Assert.AreEqual(3.33, result, 0.01);
+
+        }
+    }
+}
