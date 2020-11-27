28d5f5fa04f23c60e6ee6d0a72c339c6ee72182f
levashov_92 <levashov_92@outlook.com>
2020-11-28 00:17:51 +0300

    Create class ArrayMax

--- MainModelLibrary/ArrayMax.cs
+++ MainModelLibrary/ArrayMax.cs
@@ -2,8 +2,22 @@
 
 namespace MainModelLibrary
 {
-    public class ArrayMax
+    public static class ArrayMax
     {
+        public static int FindMax(int[] array)
+        {
+            int max = array[0];
+
+            for(int i = 0;i<array.Length;i++)
+            {
+                if (array[i] > max)
+                    max = array[i];
+            }
+
+            return max;
+        }
+
+    
 
 
     }
